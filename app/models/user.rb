000f7f754151d5ed052aaef45ee345bf86dee20f
Tuan Vu <vuttuan@tnaproduction.com>
2021-06-16 16:23:10 +0700

    No validation when updating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email, :password, :role
+  validates_presence_of :name, :email, :role
   enum role: {'student': 0, 'teacher': 1}
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   
